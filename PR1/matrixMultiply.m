5846172b50c4bd73e9300e71b62f4776503f6594
Busch, Michael <m.busch@optadata-gruppe.de>
2023-10-03 11:51:50 +0200

    matrixMultiply nochmal angepasst

--- PR1/matrixMultiply.m
+++ PR1/matrixMultiply.m
@@ -10,18 +10,14 @@ if (size(B,2) == size(C,1))
         %Gehe durch die Spalten der zweiten Matrix C, size(C,2) = Anzahl
         %der Spalten
         for h = 1 : size(C,2)
-            counter = 1;
-
             %Summiere die Werte der Zeilen von B multipliziert mit den
             %Werten der Spalten von C
             for j = 1 : length
-                    sumVektor(counter) = B(i,j) .* C(j,h);
-                    counter = counter + 1;
+                    sumVektor(j) = B(i,j) .* C(j,h);
             end
 
             A(i,h) = sum(sumVektor);
             sumVektor = zeros(1,size(B,1));
-            counter = 0;
         end
     end
 else
